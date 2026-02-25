b8fde770f61fbce11d82a29067dbb236807b737b
Ankit chhangani <155306694+ankit-chhangani@users.noreply.github.com>
2026-02-25 21:38:49 +0545

    Document various number swapping techniques
    Added comments explaining different methods to swap numbers.

--- basics/swap_num.c
+++ basics/swap_num.c
@@ -10,3 +10,28 @@ int main() {
     printf("\n the swapped number are: %d\t%d",a,b);
     return 0;
 }
+// we could swap numbers using xor which is way too cool. 
+/* int a = 2;
+    int b = 3;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+ 
+    printf("%d%d",a,b); */
+// we could swap numbers using no temporary variable using arithmetic operation.
+/*  int a = 2;
+    int b = 3;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+ 
+    printf("%d%d",a,b); */
+// we could swap numbers using pointers.
+/*  int a = 2;
+    int b = 3;
+   int *p = &a;
+   int c = *p;
+   a = b;
+   b = c;
+    printf("%d%d",a,b); */
+
